6050e55bdc71826077cc7f406aa9e088db66770f
alexei guevara <alexguev@gmail.com>
2011-05-05 00:28:51 -0400

    la1

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,20 @@
 package controllers;
-
+ 
+import java.util.*;
+ 
 import play.*;
 import play.mvc.*;
-
-import java.util.*;
-
+ 
 import models.*;
-
+ 
 public class Application extends Controller {
-
+ 
     public static void index() {
-    	System.out.println("Yop");
-        render();
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+            "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
     }
-
+ 
 }
